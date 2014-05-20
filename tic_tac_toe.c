b3eae63978791f5afe1a513d167deefa27b1884c
alin.ciobanu <alin.ciobanu@cti.pub.ro>
2014-05-20 23:35:49 +0300

    solved problem with [2][1] button

--- tic_tac_toe.c
+++ tic_tac_toe.c
@@ -13,155 +13,27 @@
 #define LROSII 12
 #define BUTOANE 13
 
+#define PLAY_WITH_HUMAN 0
+#define PLAY_WITH_BOT 2
+
 int led_verzi[N][N] = {{PC4, PC6, PA7}, {PB2, PB0, PC2}, {PA0, PA2, PA4}};
 char led_verzi_port[N][N] = {{'c', 'c', 'a'}, {'b', 'b', 'c'}, {'a', 'a', 'a'}};
 int led_rosii[N][N] = {{PC5, PC7, PA6}, {PB1, PC1, PC3}, {PA1, PA3, PA5}};
 char led_rosii_port[N][N] = {{'c', 'c', 'a'}, {'b', 'c', 'c'}, {'a', 'a', 'a'}};
-int butoane[N][N] = {{PB3, PD1, PD6}, {PB4, PD4, PD5}, {PD0, PD3, PD7}};
-char butoane_port[N][N] = {{'b', 'd', 'd'}, {'b', 'd', 'd'}, {'d', 'd', 'd'}};
+int butoane[N][N] = {{PB3, PD1, PD6}, {PB4, PD4, PD5}, {PD0, PC0, PD7}};
+char butoane_port[N][N] = {{'b', 'd', 'd'}, {'b', 'd', 'd'}, {'d', 'c', 'd'}};
 
 typedef struct Position {
 	int line;
 	int col;
 } Position;
 
-Position getPressedButton() {
-
-	int i, j;
-	Position pos;
-
-	while (1) {
-	
-		for (i = 0; i < N; i++) {
-			for (j = 0; j < N; j++) {
-				char port_letter = butoane_port[i][j];
-				int port = butoane[i][j];
-				
-				switch (port_letter) {
-					case 'b': {
-						if ((PINB & (1 << port)) == 0) {
-							pos.line = i;
-							pos.col = j;
-							return pos;
-						}
-						break;
-					}
-					case 'd': {
-						if ((PIND & (1 << port)) == 0) {
-							pos.line = i;
-							pos.col = j;
-							return pos;
-						}
-						break;
-					}
-				}				
-			}
-		}
-	}
-	
-	return pos;
-
-}
-
-void lightLEDs (int color) {
-
-	int i, j;
-	
-	for (i = 0; i < N; i++) {
-		for (j = 0; j < N; j++) {
-			char port_led;
-			int pin;
-			
-			if (color == LROSII) {
-				port_led = led_rosii_port[i][j];
-				pin = led_rosii[i][j];
-			}
-			else if (color == LVERZI) {
-				port_led = led_verzi_port[i][j];
-				pin = led_verzi[i][j];
-			}
-			
-			switch (port_led) {
-				case 'a': {
-					PORTA |= (1 << pin);
-					break;
-				}
-				case 'b': {
-					PORTB |= (1 << pin);
-					break;
-				}
-				case 'c': {
-					PORTC |= (1 << pin);
-					break;
-				}
-			}
-			_delay_ms(1000);
-		}
-	}
-
-}
-
-/**
-	Gaseste pozitia pin-ului pin in matricea specificata
-*/
-Position getPosition (int pin, int inWhatMatrix) {
-
-	int i, j;
-	Position pos;
-
-	switch (inWhatMatrix) {
-		
-		case LVERZI: {
-			for (i = 0; i < N; i++) {
-				for (j = 0; j < N; j++) {
-					if (led_verzi[i][j] == pin) {
-						pos.line = i;
-						pos.col = j;
-						return pos;
-					}
-				}
-			}
-			break;
-		}
-
-		case LROSII: {
-			for (i = 0; i < N; i++) {
-				for (j = 0; j < N; j++) {
-					if (led_rosii[i][j] == pin) {
-						pos.line = i;
-						pos.col = j;
-						return pos;
-					}
-				}
-			}
-			break;
-		}
-
-		case BUTOANE: {
-			for (i = 0; i < N; i++) {
-				for (j = 0; j < N; j++) {
-					if (butoane[i][j] == pin) {
-						pos.line = i;
-						pos.col = j;
-						return pos;
-					}
-				}
-			}
-			break;
-		}
-
-	}
-	
-	return pos;
-
-}
-
 void init () {
 
 	/* setare butoane ca intrare */
 	DDRD &= ~(1 << PD0);
 	DDRD &= ~(1 << PD1);
-	DDRD &= ~(1 << PD3);
+	DDRC &= ~(1 << PC0);
 	DDRD &= ~(1 << PD4);
 	DDRD &= ~(1 << PD5);
 	DDRD &= ~(1 << PD6);
@@ -171,7 +43,7 @@ void init () {
 	/* activare rezistenta de pull-up pentru butoane */
 	PORTD |= (1 << PD0);
 	PORTD |= (1 << PD1);
-	PORTD |= (1 << PD3);
+	PORTC |= (1 << PC0);
 	PORTD |= (1 << PD4);
 	PORTD |= (1 << PD5);
 	PORTD |= (1 << PD6);
@@ -221,19 +93,111 @@ void init () {
 
 }
 
-void start() {
-	
-	init();
-	lightLEDs(LROSII);
-	
-/*
+Position getPressedButton() {
+
+	int i, j;
+	Position pos;
+
 	while (1) {
-		Position pos = getPressedButton();
+	
+		for (i = 0; i < N; i++) {
+			for (j = 0; j < N; j++) {
+				char port_letter = butoane_port[i][j];
+				int port = butoane[i][j];
+				
+				switch (port_letter) {
+					case 'b': {
+						if ((PINB & (1 << port)) == 0) {
+							pos.line = i;
+							pos.col = j;
+							return pos;
+						}
+						break;
+					}
+					case 'c': {
+						if ((PINC & (1 << port)) == 0) {
+							pos.line = i;
+							pos.col = j;
+							return pos;
+						}
+						break;
+					}
+					case 'd': {
+						if ((PIND & (1 << port)) == 0) {
+							pos.line = i;
+							pos.col = j;
+							return pos;
+						}
+						break;
+					}
+				}				
+			}
+		}
+	}
+	
+	return pos;
+
+}
+
+void loadingWithLEDs (int color) {
+
+	int i, j;
+	
+	for (i = 0; i < N; i++) {
+		for (j = 0; j < N; j++) {
+			char port_led;
+			int pin;
+			
+			if (color == LROSII) {
+				port_led = led_rosii_port[i][j];
+				pin = led_rosii[i][j];
+			}
+			else if (color == LVERZI) {
+				port_led = led_verzi_port[i][j];
+				pin = led_verzi[i][j];
+			}
+			
+			switch (port_led) {
+				case 'a': {
+					PORTA |= (1 << pin);
+					break;
+				}
+				case 'b': {
+					PORTB |= (1 << pin);
+					break;
+				}
+				case 'c': {
+					PORTC |= (1 << pin);
+					break;
+				}
+			}
+			_delay_ms(100);
+		}
+	}
+	_delay_ms(500);
+	init();
+
+}
+
+void start() {
+
+	init();
+	loadingWithLEDs(LVERZI);
+	Position pos = getPressedButton();
+	if (pos.col == PLAY_WITH_HUMAN) {
+		loadingWithLEDs(LVERZI);
+	}
+	else if (pos.col == PLAY_WITH_BOT) {
+		loadingWithLEDs(LROSII);
+	}
+
+	while (1) {
+		pos = getPressedButton();
 		
-		char port_led_rosu = led_rosii_port[pos.line][pos.col];
-		int pin = led_rosii[pos.line][pos.col];
+		char port_led = led_verzi_port[pos.line][pos.col];
+		int pin = led_verzi[pos.line][pos.col];
 		
-		switch (port_led_rosu) {
+		switch (port_led) {
 			case 'a': {
 				PORTA |= (1 << pin);
 				break;
@@ -248,7 +212,6 @@ void start() {
 			}
 		}
 	}
-*/
 
 }
 
